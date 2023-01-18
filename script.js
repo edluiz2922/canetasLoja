30d1bbac3da4db445b551bad4ac48a948c597b9e
edLuiz <100592220+edluiz2922@users.noreply.github.com>
2023-01-17 21:25:10 -0300

    atualização js
    atualização 2

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-const imgs = document.getElementById("img");
-const img = document.querySelector("#fig img");
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-let idx = 0;
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navbar.classList.toggle('active');
+};
 
-function carrossel(){
-    idx++;
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navbar.classList.remove('active');
+};
 
-    if(idx > img.length-1){
-        idx = 0;
+//carrossel
+let time = 2000,
+	currentImageIndex = 0,
+	imagens = document
+			.querySelectorAll("#slider img"),
+	max = imagens.length
 
-    }
+function nextImage(){
+	//console.logo("imagens");
+	imagens[currentImageIndex]
+		.classList.remove("selected")
 
-    imgs.style.transform = 'translateX( ${-idx *480}px)'
+	currentImageIndex++;
+
+	if(currentImageIndex >=max){
+		currentImageIndex=0;
+	}
+
+	imagens[currentImageIndex]
+		.classList.add("selected")
+
 }
+function start(){	
+	setInterval(()=> {
+		//console.log("funcao rodada a 2s");
+		nextImage();
+	},time)
+} // fim da função start
 
-setInterval(carrossel, 1800);
+window.addEventListener("load", start);
+
